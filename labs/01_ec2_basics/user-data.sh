e16b9fdea33895fd0f934d5fd22249ac882bd348
Patrick <pcallaway0603@gmail.com>
2025-10-29 19:38:21 -0400

    AWS BASICS - LAB01

---
+++ labs/01_ec2_basics/user-data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euxo pipefail
+dnf -y update
+dnf -y install nginx
+systemctl enable nginx
+cat > /usr/share/nginx/html/index.html << 'HTML'
+<!DOCTYPE html>
+<html lang="en">
+<head><title>Lab 01</title></head>
+<body>
+<h1> It works! </h1>
+<p> This page was provisioned via EC2 User Data script. </p>
+</body>
+</html>
+HTML
+systemctl start nginx
+
+    
